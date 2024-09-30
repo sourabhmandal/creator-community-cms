c99a4b6c34642b88c07933efde9c885d942b0e1c
sourabhmandal <19mandal97@gmail.com>
2024-09-30 23:32:45 +0530

    init springboot project

---
+++ src/test/java/com/nxtweb/supareel/SupareelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nxtweb.supareel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupareelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
