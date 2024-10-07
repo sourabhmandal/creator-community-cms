9b33ab69e35c200fe0e3af29f505ea6dc623cd55
sourabhmandal <19mandal97@gmail.com>
2024-10-07 23:12:02 +0530

    add file upload api

--- src/main/java/com/nxtweb/supareel/product/ProductController.java
+++ src/main/java/com/nxtweb/supareel/product/ProductController.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping(name = "products")
 @RequiredArgsConstructor
-@Tag(name = "Book")
+@Tag(name = "Product")
 public class ProductController {
     private final ProductService service;
 
